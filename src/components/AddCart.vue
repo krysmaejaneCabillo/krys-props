83f568d92d06f09ecd6803560c9b6ed5d603ee09
krysmaejaneCabillo <ckrysmaejane@gmail.com>
2026-06-02 09:28:49 +0800

    product

--- src/components/AddCart.vue
+++ src/components/AddCart.vue
@@ -14,18 +14,7 @@ export default {
             total: "0.00"
         };
     },
-        computed: {
-        filteredProducts() {
-            if (!this.searchBar.trim) {
-                return this.products
-            }
-            const query = this.searchBar.toLowerCase().trim();
-            return this.products.filter(product => 
-                product.name.toLowerCase().includes(query)
-            )
-        }
-
-    },
+    
     methods: {
         removeItem(id) {
           
